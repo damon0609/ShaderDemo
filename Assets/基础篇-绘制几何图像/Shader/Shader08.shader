318df016c9e295ea55a7ee45e75c740bfbab6af1
damon <damon.cheng@picovr.com>
2020-04-16 19:09:17 +0800

    放大镜功能完成

--- Assets/基础篇-绘制几何图像/Shader/Shader08.shader
+++ Assets/基础篇-绘制几何图像/Shader/Shader08.shader
@@ -3,7 +3,9 @@
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
-        _ScaleFactor("ScaleFactor",Range(0,1))=0.5
+        //_Pos("Pos",Vector) = (0.5,0.5,0,0)
+        // _Radius("Radius",Range(0,1)) = 0
+        // _ScaleFactor("ScaleFactor",Range(0,1))=0.2
     }
     SubShader
     {
@@ -30,8 +32,10 @@
 
             sampler2D _MainTex;
             float4 _MainTex_ST;
-            fixed4 _pos;
-            fixed _ScaleFactor;
+            fixed4 _Pos;
+            float _Radius;
+            float _ScaleFactor;
+            float _EdgeSize;
 
             v2f vert (appdata v)
             {
@@ -40,11 +44,15 @@
                 o.uv = v.uv;
                 return o;
             }
-
             fixed4 frag (v2f i) : SV_Target
             {
-                fixed2 dir =(_pos.xy + fixed2(0.5,0.5))-i.uv;
-                fixed4 col = tex2D(_MainTex, i.uv+dir*_ScaleFactor);
+                float2 scale = float2(_ScreenParams.x/_ScreenParams.y,1);//这里为什么会使用_ScreenParams.x/_ScreenParams.y
+                float2 center = _Pos;
+                float2 dir =center - i.uv;
+                float dis = length(dir*scale);
+                //float num =1 - step(_Radius,dis);
+                float num = smoothstep(_Radius+_EdgeSize,_Radius,dis);
+                fixed4 col = tex2D(_MainTex, i.uv+dir*_ScaleFactor*num);
                 return col;
             }
             ENDCG
--- Assets/基础篇-绘制几何图像/ZTest/PostEffectBase.cs
+++ Assets/基础篇-绘制几何图像/ZTest/PostEffectBase.cs
@@ -1,28 +1,87 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PostEffectBase : MonoBehaviour {
-    [SerializeField]
-    Material material;
-    void Start () {
 
+    public Shader zoomShader;
+
+    Material material;
+    private Vector2 pos = new Vector4 (0.0f, 0.0f);
+    private Vector2 target;
+
+    private Vector2 dir;
+
+    [SerializeField]
+    [Range (0, 1)]
+    private float mScaleFactor;
+
+    [SerializeField]
+    [Range (0, 1)]
+    private float mScaleArea;
+
+    [SerializeField]
+    [Range (0, 1)]
+    private float mEdgeSize;
+    private float scaleValue;
+
+    public SpriteRenderer sprite;
+
+    private Material CheckShaderAndMaterial (Shader shader) {
+        Material mat = null;
+        if (shader == null || !shader.isSupported)
+            mat = null;
+        else {
+            mat = new Material (shader);
+            mat.hideFlags = HideFlags.DontSave;
+        }
+        return mat;
+    }
+
+    void Start () {
+        material = CheckShaderAndMaterial (zoomShader);
+        if (material != null) {
+            material.SetFloat ("_ScaleFactor", mScaleFactor);
+            material.SetFloat ("_Radius", mScaleArea);
+            material.SetFloat ("_EdgeSize", mEdgeSize);
+        }
+        target = new Vector2 (Screen.width / 2, Screen.height / 2);
+        dir = target - pos;
     }
 
     void Update () {
-        if (Input.GetMouseButtonDown (0)) {
-            Vector2 pos = Input.mousePosition;
-            float x = pos.x / Screen.width;
-            float y = pos.y / Screen.height;
-
-            material.SetVector ("_pos", new Vector4 (x, y));
-            Debug.Log ("--" + x + "--" + y);
+        if (Vector2.Distance (target, pos) <= 5f) {
+            float xPos = UnityEngine.Random.Range (0, Screen.width);
+            float yPos = UnityEngine.Random.Range (0, Screen.height);
+            target = new Vector2 (xPos, yPos);
+            dir = target - pos;
         }
+        pos += dir.normalized * Time.deltaTime * 300;
+
+        scaleValue += Input.GetAxis ("Mouse ScrollWheel");
+        scaleValue = Mathf.Clamp (scaleValue, -1, 1);
     }
 
     private void OnRenderImage (RenderTexture src, RenderTexture dest) {
         if (material != null) {
+            Vector2 temp = new Vector2 (pos.x / Screen.width, pos.y / Screen.height);
+            material.SetVector ("_Pos", temp);
+            material.SetFloat ("_ScaleFactor", mScaleFactor + scaleValue);
+            material.SetFloat ("_Radius", mScaleArea);
+            material.SetFloat ("_EdgeSize", mEdgeSize);
             Graphics.Blit (src, dest, material);
+        } else
+            Graphics.Blit (src, dest);
+    }
+    private void OnDestroy () {
+        if (material != null) {
+            material.SetFloat ("_Radius", 0);
+            material.SetFloat ("_ScaleFactor", 0);
+            material.SetFloat ("_Radius", 0);
+            material.SetFloat ("_EdgeSize", 0);
+            pos = new Vector4 (0f, 0f);
         }
     }
 }
