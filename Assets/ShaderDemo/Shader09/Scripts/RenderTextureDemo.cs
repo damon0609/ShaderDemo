5dbfa00b99805aa8b65de5bd5a6980c43b7ff943
damon0609 <903005766@qq.com>
2020-09-16 23:23:54 +0800

    添加commandBuffer

--- Assets/ShaderDemo/Shader09/Scripts/RenderTextureDemo.cs
+++ Assets/ShaderDemo/Shader09/Scripts/RenderTextureDemo.cs
@@ -3,105 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-[ExecuteInEditMode]
-public class RenderTextureDemo : MonoBehaviour
-{
+public class RenderTextureDemo : MonoBehaviour {
     private Renderer targetRenderer;
     private RenderTexture rt;
     private CommandBuffer commandBuffer;
-    public Material material;
-    public Material effectMat;
+    public Material replaceMat;
 
-    public Color outLineColor;
-    public int outLineSize;
+    private void OnEnable () {
+        targetRenderer = gameObject.GetComponent<Renderer> ();
 
-    public int blurSize = 2;
+        if (commandBuffer == null) {
+            commandBuffer = new CommandBuffer ();
+            commandBuffer.name = "test";
 
-    public GameObject targetObj;
+            rt = new RenderTexture (512, 512, 0, RenderTextureFormat.ARGB32);
+            commandBuffer.SetRenderTarget (rt);
+            commandBuffer.ClearRenderTarget (true, true, Color.gray);
 
-    private void OnEnable()
-    {
-        if (rt != null)
-        {
-            RenderTexture.ReleaseTemporary(rt);
-            rt = null;
+            Material mat = replaceMat == null? targetRenderer.sharedMaterial : replaceMat;
+            mat.mainTexture = rt;
+            commandBuffer.DrawRenderer (targetRenderer, mat);
+            Camera.main.AddCommandBuffer (CameraEvent.AfterForwardOpaque, commandBuffer);
         }
-        if (commandBuffer != null)
-        {
-            commandBuffer.Release();
-            commandBuffer = null;
-        }
-    }
-    void Start()
-    {
-        Renderer renderer = targetObj.GetComponent<Renderer>();
-        if (material != null)
-        {
-            commandBuffer = new CommandBuffer();
-            commandBuffer.name = "ReplaceMat";
-            rt = RenderTexture.GetTemporary(512, 512, 0);
-            commandBuffer.SetRenderTarget(rt);
-            commandBuffer.ClearRenderTarget(true, true, Color.gray);
-            commandBuffer.DrawRenderer(renderer, material);
-            renderer.sharedMaterial.mainTexture = rt;
-            Camera.main.AddCommandBuffer(CameraEvent.AfterForwardOpaque, commandBuffer);
-        }
-        else
-        {
-            enabled = false;
-        }
-    }
-
-    private void OnRenderImage(RenderTexture src, RenderTexture dest)
-    {
-        if (rt != null && material != null && commandBuffer != null)
-        {
-            material.SetColor("_OutLineColor", outLineColor);
-            // Graphics.ExecuteCommandBuffer(commandBuffer);
-            Graphics.Blit(rt, dest);
-
-            // RenderTexture temp1 = RenderTexture.GetTemporary(src.width, src.height, 0);
-            // RenderTexture temp2 = RenderTexture.GetTemporary(src.width, src.height, 0);
-
-            // effectMat.SetInt("_outLineSize", outLineSize);
-
-            // Graphics.Blit(rt, temp1, effectMat, 0);
-            // Graphics.Blit(temp1, temp2, effectMat, 1);
-
-            // for (int i = 0; i < blurSize; i++)
-            // {
-            //     Graphics.Blit(temp2, temp1, effectMat, 0);
-            //     Graphics.Blit(temp1, temp2, effectMat, 1);
-            // }
-
-            // effectMat.SetTexture("_renderTex", rt);
-            // Graphics.Blit(temp2, temp1, effectMat, 2);
-
-            // effectMat.SetTexture("_outLineTex", temp1);
-            // Graphics.Blit(src, dest, effectMat, 3);
-
-            // RenderTexture.ReleaseTemporary(temp1);
-            // RenderTexture.ReleaseTemporary(temp2);
-        }
-        else
-        {
-            Graphics.Blit(src, dest);
-        }
-    }
-    void Update()
-    {
 
     }
 
-    private void OnDisable()
-    {
-
-        if (commandBuffer != null)
-        {
-            Camera.main.RemoveCommandBuffer(CameraEvent.AfterForwardOpaque, commandBuffer);
-            commandBuffer.Clear();
+    private void OnDestroy () {
+        if (commandBuffer != null) {
+            Camera.main.RemoveCommandBuffer (CameraEvent.AfterForwardOpaque, commandBuffer);
+            commandBuffer.Clear ();
+            rt.Release ();
         }
-        if (rt != null)
-            rt.Release();
     }
 }
