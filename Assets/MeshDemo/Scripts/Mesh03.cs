2daabafea409705f207083c4961f0a334db185f8
damon0609 <43327994+damon0609@users.noreply.github.com>
2020-10-30 10:47:56 +0800

    10.30

--- Assets/MeshDemo/Scripts/Mesh03.cs
+++ Assets/MeshDemo/Scripts/Mesh03.cs
@@ -1,33 +1,58 @@
 ﻿using UnityEngine;
-
-[ExecuteInEditMode]
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Mesh03 : MonoBehaviour
 {
     public Vector3 p1;
     public Vector3 p2;
     public Vector3 p3;
-
-    private Matrix4x4 matrix;
-    private Vector3 p11;
-    private Vector3 p22;
-    private Vector3 p33;
+    public Vector3 p4;
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(p11, 0.1f);
-        Gizmos.DrawSphere(p22, 0.1f);
-        Gizmos.DrawSphere(p33, 0.1f);
-    }
+        if (meshFilter != null)
+        {
+            Gizmos.color = Color.red;
+            Mesh mesh = meshFilter.sharedMesh;
+            Vector3[] list = mesh.vertices;
+            for (int i = 0; i < list.Length; i++)
+            {
+                Gizmos.DrawSphere(transform.TransformPoint(list[i]), 0.1f);
+            }
+            Gizmos.color = Color.blue;
+            Vector3[] normals = mesh.normals;
+            for (int j = 0; j < normals.Length; j++)
+            {
+                Vector3 start = transform.TransformPoint(list[j]);//世界空间下顶点坐标
+                Vector3 end = start + transform.TransformDirection(normals[j]);//世界空间下法线
+                Gizmos.DrawLine(start, end);
+            }
 
+            Gizmos.color = Color.yellow;
+            Vector4[] tangents = mesh.tangents;
+            for (int m = 0; m < tangents.Length; m++)
+            {
+                Vector4 point = transform.TransformDirection(tangents[m]).normalized;
+                Vector3 start = transform.TransformPoint(list[m]);//世界空间下顶点坐标
+                Gizmos.DrawLine(start, start+ new Vector3(point.x,point.y,point.z));
+            }
+            Gizmos.color = Color.green;
+            for (int index = 0; index < normals.Length; index++)
+            {
+                Vector3 n = transform.TransformDirection(normals[index]);
+                Vector3 t = transform.TransformDirection(tangents[index]);
+                Vector3 b = Vector3.Cross(n,t)*tangents[index].w;//副切线
+
+                Vector3 start = transform.TransformPoint(list[index]);//世界空间下顶点坐标
+                Gizmos.DrawLine(start, start + b);
+            }
+        }
+    }
     private MeshRenderer meshRenderer;
     private MeshFilter meshFilter;
     private Mesh tempMesh;
-
     private void Start()
     {
-      
+
     }
     [ContextMenu("GenerateMesh")]
     void GenerateMesh()
@@ -35,20 +60,15 @@ public class Mesh03 : MonoBehaviour
         meshFilter = transform.GetComponent<MeshFilter>();
         meshRenderer = transform.GetComponent<MeshRenderer>();
 
-        matrix = transform.localToWorldMatrix;
-        p11 = matrix.MultiplyPoint(p1);
-        p22 = matrix.MultiplyPoint(p2);
-        p33 = matrix.MultiplyPoint(p3);
-
         tempMesh = new Mesh();
         tempMesh.name = "tempMesh";
-        tempMesh.vertices = new Vector3[3] { p11, p22, p33 };
-        tempMesh.triangles = new int[3] { 0, 1, 2 };
+        tempMesh.vertices = new Vector3[4] { p1, p2, p3, p4 };
+        tempMesh.triangles = new int[6] { 0, 3, 1, 1, 3, 2 };
+        tempMesh.uv = new Vector2[4] { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1),new Vector2(0,1) };
         tempMesh.RecalculateNormals();
         tempMesh.RecalculateBounds();
         tempMesh.RecalculateTangents();
 
         meshFilter.mesh = tempMesh;
     }
-   
 }
